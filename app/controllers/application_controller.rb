72a32bc0182e5ccebeac72ab029c816ff5ddc942
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-18 11:32:15 -0800

    commit tree e58f1bede2cecf32c37337828142104e5c7a75ac

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,17 +11,31 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
+    @user = User.find_by(username: params[:username])
+
+    if @user != nil && @user.password == params[:password]  #if conditions are met
+      session[:user_id] = @user.id                          #match info from database
+      redirect '/account'                                   #redirect to account route
+    end 
+
+    erb :error                                              #if not render error page
 
   end
 
   get '/account' do
+    @current_user = User.find_by_id(session[:user_id])     #find current user set to session id
+    if @current_user                                       # if user in session database 
+      erb :account                                         # render account page 
+    else 
+      erb :error                                           # else, render error page
+    end 
 
   end
 
-  get '/logout' do
+  get '/logout' do                       #clear session, and redirect back to home
+    session.clear 
 
+    redirect '/'
   end
-
-
 end
 
